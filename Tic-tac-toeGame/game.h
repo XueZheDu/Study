6924e82bed6be538ecffb4f10fba007f3b8b0534
XueZheDu <161428358+XueZheDu@users.noreply.github.com>
2024-03-13 18:01:26 +0800

    Add files via upload

--- Tic-tac-toeGame/game.h
+++ Tic-tac-toeGame/game.h
@@ -1,1 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#define ROW 3
+#define COL 3
+
+//ÉùÃ÷
+void test();
+void menu();
+void game();
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+char IsWin(char board[ROW][COL], int row, int col);
+int IsFull(char board[ROW][COL], int row, int col);
